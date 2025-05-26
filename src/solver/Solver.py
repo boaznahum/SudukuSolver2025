f282e6dd6203d71e364270e07fa2d53632a50aef
Boaz Nahum <boaznahum@gmail.com>
2025-05-26 23:12:47 +0300

    Solve algorithm  still not works

--- src/solver/Solver.py
+++ src/solver/Solver.py
@@ -1,15 +1,19 @@
+import enum
 from typing import Generator
 
 from data import SudokuBoard
 from data.Cell import Cell
 
 
-# class State(enum.Enum):
-#     IDLE = "idle"
-#     NOTES = "creating_notes"
-#     REPLACE_SINGLES = "replace_singles"
-#     SOLVING = "solving"
-#     SOLVED = "solved"
+class SolveResult(enum.Enum):
+     SOLVED = "solved"
+     NOT_SOLVED_CONTINUE = "not_solved_continue"
+     NOT_SOLVED_INVALID = "not_solved_invalid"
+
+class UpdateResult(enum.Enum):
+     CELL_WITH_NO_NOTES = "cell_with_no_notes"
+     ALL_VALUES = "all_values"
+     SOME_CELLS_WITH_NOTES = "some_cells_with_notes"
 
 
 class Solver:
@@ -35,34 +39,54 @@ class Solver:
         # if we in state NOTES, we advance to state SOLVING
         #  when we finish we move to state SOLVED
 
-        inner_solver = self._solve()
+        inner_solver: Generator[SolveResult, None, None] = self._solve()
 
         while True:
 
 
-            solved = next(inner_solver)
+            solved_status: SolveResult = next(inner_solver)
+            solved = solved_status == SolveResult.SOLVED
 
             do_continue = yield solved
             if solved:
-                print("Sudoku solved, moving to state SOLVED.")
+                print("Sudoku solved, Done.")
+                return None
+
+            if solved_status == SolveResult.NOT_SOLVED_INVALID:
+                print("Sudoku is not solvable, Done.")
                 return None
 
             if not do_continue:
                 print("Stopping the solving process.")
                 return None
 
-    def _update_notes(self):
+    def _update_notes(self) -> UpdateResult:
         """
         Update notes for all cells in the Sudoku board.
+        return the number of cells that have no value and where notes were updated.
         """
         print("Updating notes for all cells.")
+        number_of_cells_with_notes: int = 0
         for row in range(9):
             for col in range(9):
-                self._update_cell_notes(row, col)
+                update_note_result = self._update_cell_notes(row, col)
 
-    def _update_cell_notes(self, cell_row, cell_col) -> None:
+                if update_note_result >= 0: # a cell with notes
+                    if update_note_result == 0:
+                        return UpdateResult.CELL_WITH_NO_NOTES  # invalid abort the process
+                    else:
+                        number_of_cells_with_notes += 1
+
+        if number_of_cells_with_notes == 0:
+            return UpdateResult.ALL_VALUES
+        else:
+            return UpdateResult.SOME_CELLS_WITH_NOTES
+
+    def _update_cell_notes(self, cell_row, cell_col) -> int | None:
         """
         Update the notes for the current cell.
+        return the number of notes in cell
+        if less than zero mean it is cell with value
         """
 
         cell: Cell = self._board.get_cell(cell_row, cell_col)
@@ -70,37 +94,102 @@ class Solver:
         value = cell.get_value()
 
         if value is not None:
-            return
+            return -1
 
         # If the cell is empty, we can set some notes
 
         cells_in_areas: list[int] = self._board.get_values_in_3_areas(cell_row, cell_col)
+        number_of_notes: int = 0
         for x in range(1, 10):
             # for current row, col , get all cells in row current_row, in col_current_col, and 3x3 board that contains
             #             # the current cell
             if x not in cells_in_areas:
                 cell.set_note(x)
+                number_of_notes += 1
             else:
                 cell.clear_note(x)
 
-    def _solve(self) -> Generator[bool, bool, None] :
+        return number_of_notes
+
+    def _solve(self) -> Generator[SolveResult, None, None] :
         print("Actually Solving Sudoku...")
 
 
         while True:
 
-            self._update_notes()
-            yield False
+            update_notes_result = self._update_notes()
+            if  update_notes_result == UpdateResult.CELL_WITH_NO_NOTES:
+                print("Found a cell with no notes, aborting.")
+                yield SolveResult.NOT_SOLVED_INVALID
+            elif update_notes_result == UpdateResult.ALL_VALUES:
+                # if all cells have values, we can assume that the Sudoku is solved
+                print("All cells have notes, Sudoku is solved.")
+                yield SolveResult.SOLVED
+                return
+            else:
+                yield SolveResult.NOT_SOLVED_CONTINUE
 
             # search for a cell with a singe not, and set the value of this cell to this note
             if self._replace_single_note_cells():
                 print("Found a single note cell and replaced it.")
                 # after replacing a single note cell, we need to update notes for all cells
                 # this will occur in next iteration of the generator
-                yield False
+                yield SolveResult.NOT_SOLVED_CONTINUE
             else:
-                print("No single note cell found, stopping the solving process.")
-                yield True  # no single note cell found, we stop the solving process
+
+                # try to solve by recursion
+                # now search for a cell with notes and try to replace it with a value
+                print("No single note cell found, trying to replace values.")
+                yield SolveResult.NOT_SOLVED_CONTINUE  # no single note cell found, we stop the solving process
+
+                # search for a cell with no value but with notes
+                # if found a cell with no notes abort the process
+                for row in range(9):
+                    for col in range(9):
+                        cell: Cell = self._board.get_cell(row, col)
+                        # if the cell has a value, we skip it
+                        if cell.get_value() is not None:
+                            continue
+                        notes = cell.get_notes()
+                        if notes.count(None) == 9:
+                            print(f"Found a cell with no notes at ({row}, {col}), aborting.")
+                            yield SolveResult.NOT_SOLVED_INVALID
+                            return
+
+                        # if the cell has notes, we try to replace it with a value
+                        else:
+                            # we can replace this cell with a value
+                            for note in notes:
+                                if note is not None:
+                                    # set the value of the cell to this note
+                                    cell.set_value(note)
+                                    print(f"Found a note cell at ({row}, {col} ) putting {note}.")
+
+                                    for now_solved in self._solve():
+                                        if now_solved == SolveResult.SOLVED:
+                                            print("Sudoku solved after replacing a note cell.")
+                                            yield SolveResult.SOLVED
+                                            return
+                                        elif now_solved == SolveResult.NOT_SOLVED_CONTINUE:
+                                            yield SolveResult.NOT_SOLVED_CONTINUE
+                                            # and continue next iteration
+                                        else:
+                                            assert now_solved == SolveResult.NOT_SOLVED_INVALID
+
+                                            # restore the value and continue solving
+                                            cell.set_value(None)
+                                            # break the generator
+                                            break
+                                    # not solved, restore the value and continue solving
+                                    print(f"*** Was not able to solve with {note} in cell at ({row}, {col} ), restoring")
+                                    yield SolveResult.NOT_SOLVED_CONTINUE
+                                    cell.set_value(None)
+                                    self._update_notes()
+
+
+
+                # if we reached this point, it means that we didn't find a solution
+                yield SolveResult.NOT_SOLVED_CONTINUE
                 # exit the generator
                 return
 
